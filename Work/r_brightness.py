9a8f1ea02d21bcc1c39740f764b65732232a671e
Edgar Escalante <edgar.escalan@gmail.com>
2017-04-29 19:54:39 -0700

    working on brightness matrix in 2d

--- Work/r_brightness.py
+++ Work/r_brightness.py
@@ -2,12 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 a = 50
-b = []
+b = [[],[]]
 c = []
 
-for r in range(51):
-   I = 1-0.6096*(1-np.sqrt((a**2 - r**2)/a**2))
-   b.append(I)
+for i in range(51):
+    for j in range(51):
+        if (i**2 + j**2)>a**2:
+            I = 0
+            b.append(I)
+        I = 1-0.6096*(1-np.sqrt((a**2 - (i**2 + j**2))/a**2))
+        b.append(I)
 #print(b)
 
 c = b[::-1] + b[1::]
