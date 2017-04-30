d91c7e2def8fa4bafdb5eb69de75d592cc289f52
Edgar Escalante <edgar.escalan@gmail.com>
2017-04-29 18:36:37 -0700

    brightness ray fixed

--- Work/r_brightness.py
+++ Work/r_brightness.py
@@ -3,11 +3,16 @@ import matplotlib.pyplot as plt
 
 a = 50
 b = []
+c = []
 
 for r in range(51):
    I = 1-0.6096*(1-np.sqrt((a**2 - r**2)/a**2))
    b.append(I)
+#print(b)
 
-print(b)
+c = b[::-1] + b[1::]
 
-plt.plot()
+print(c, len(c))
+
+plt.plot(c)
+plt.show()
