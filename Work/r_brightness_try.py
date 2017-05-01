d2d428364634aa6dfb00b0683aa5720c9cb51273
TianchengGong <tgong4@asu.edu>
2017-05-01 08:37:26 +0800

    fix nan in old file

--- Work/r_brightness_try.py
+++ Work/r_brightness_try.py
@@ -2,24 +2,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 a = 50
-b = np.zeros((51,51), dtype=np.float64)
-print(b, b.shape)
+b = [[],[]]
 c = []
 
 for i in range(51):
     for j in range(51):
         if (i**2 + j**2)>a**2:
+            I = 0
+            b.append(I)
             break
         else:
             I = 1-0.6096*(1-np.sqrt((a**2 - (i**2 + j**2))/a**2))
-            b[[i],[j]] = I
-            #b.append(I)
-print()
-print(b)
+            b.append(I)
+#print(b)
 
 c = b[::-1] + b[1::]
 
-#print(c, len(c))
-
-plt.plot(c)
-plt.show()
+print(c)
+#print(b)
+#plt.plot(c)
+#plt.show()
