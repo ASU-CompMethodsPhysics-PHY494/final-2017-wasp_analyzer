e1b4ca30c4169a3b97ca7cf3d60b9d6c8a4f1d20
Edgar Escalante <edgar.escalan@gmail.com>
2017-04-30 17:15:19 -0700

    testing some more files for Gong

--- Work/r_brightness.py
+++ Work/r_brightness.py
@@ -2,21 +2,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 a = 50
-b = [[],[]]
+b = np.zeros((51,51), dtype=np.float64)
+print(b, b.shape)
 c = []
 
 for i in range(51):
     for j in range(51):
         if (i**2 + j**2)>a**2:
-            I = 0
-            b.append(I)
-        I = 1-0.6096*(1-np.sqrt((a**2 - (i**2 + j**2))/a**2))
-        b.append(I)
-#print(b)
+            break
+        else:
+            I = 1-0.6096*(1-np.sqrt((a**2 - (i**2 + j**2))/a**2))
+            b[[i],[j]] = I
+            #b.append(I)
+print()
+print(b)
 
 c = b[::-1] + b[1::]
 
-print(c, len(c))
+#print(c, len(c))
 
 plt.plot(c)
 plt.show()
